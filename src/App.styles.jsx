aa44c25460f1fc65c3ac3b85bcd9a0dbe6bf1bba
ajs3801 <tt14an@gmail.com>
2023-12-28 14:31:19 +0900

    fix height

--- src/App.styles.jsx
+++ src/App.styles.jsx
@@ -2,7 +2,7 @@ import styled from "@emotion/styled";
 
 export const AppContainer = styled.div`
   max-width: 400px;
-  height: calc(var(--vh, 1vh) * 100);
+  /* height: calc(var(--vh, 1vh) * 100); */
   margin: auto;
   display: flex;
   justify-content: center;
